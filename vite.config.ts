65dc1c7b9aafc25e0afe2ee98faaaddc158da7f3
Arsenios Hadjikyriacou <arsenhad@gmail.com>
2025-02-18 12:10:52 +0200

    Deleted gh-pages.yml workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { fileURLToPath } from "url";
-import path from "path";
+// import { fileURLToPath } from "url";
+// import path from "path";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "~": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
-    },
-  },
-  base: process.env.NODE_ENV === "production" ? "/cymathsin/" : "",
+  base: "/cymathsin/",
+  // resolve: {
+  //   alias: {
+  //     "~": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+  //   },
+  // },
+  // base: process.env.NODE_ENV === "production" ? "/cymathsin/" : "",
 });
